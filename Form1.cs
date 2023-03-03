381502b9422ea27a736eb2a17fdb687f3237a9b5
TrewCar <113162792+TrewCar@users.noreply.github.com>
2023-03-03 21:32:54 +0300

    Убрано лишнее

--- Form1.cs
+++ Form1.cs
@@ -13,7 +13,7 @@ public partial class Form1 : Form
 
         timer1.Start();     
     }
-    int RectSZ = 10; // cell size
+    int RectSZ = 10; // cell size 
 
     int i = 0; // the initial frame
     private void timer1_Tick(object sender, EventArgs e)
@@ -38,7 +38,7 @@ public partial class Form1 : Form
         pictureBox1.Image = ttt.CreateGridFromIMG(ttt.Width / RectSZ, ttt.Height / RectSZ).CreateIMG(RectSZ, ttt.Size);
         pictureBox1.Invalidate();
 
-        //pictureBox1.Image.Save($@"D:\Bad Apple 2\BadApple{i}.png"); //To save frames
+        //pictureBox1.Image.Save($@"D:\Bad Apple\BadApple{i}.png"); //To save frames
     }
     private string Tototott(int i )
     {
